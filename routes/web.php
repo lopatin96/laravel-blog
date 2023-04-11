6a50ae626e7dd9a4ff820d59721b805507b6b950
vladyslavlopatin <ilopatin3@gmail.com>
2023-04-11 23:06:58 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('blog', Atin\LaravelBlog\Http\Controllers\PostController::class)
     ->only(['index', 'show']);
+
 Route::get('/blog/{post}/image', [Atin\LaravelBlog\Http\Controllers\PostController::class, 'image']);
