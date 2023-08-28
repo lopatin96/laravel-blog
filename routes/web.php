3b49a757ad2b0258a5a7245829e2c3b8d0574de6
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-08-28 12:34:01 +0200

    web middleware

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::resource('blog', Atin\LaravelBlog\Http\Controllers\PostController::class)
-    ->only(['index', 'show']);
+Route::middleware(['web'])->group(function () {
+    Route::resource('blog', Atin\LaravelBlog\Http\Controllers\PostController::class)
+        ->only(['index', 'show']);
 
-Route::get('/blog/{post}/image', [Atin\LaravelBlog\Http\Controllers\PostController::class, 'image']);
+    Route::get('/blog/{post}/image', [Atin\LaravelBlog\Http\Controllers\PostController::class, 'image']);
+});
