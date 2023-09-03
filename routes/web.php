b13ea5da82039ca9a7ab81d1ee677b3469517a87
vladyslavlopatin <ilopatin3@gmail.com>
2023-09-03 12:57:36 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::domain(env('APP_URL'))->middleware(['web'])->group(function () {
-    Route::resource('blog', Atin\LaravelBlog\Http\Controllers\PostController::class)
-        ->only(['index', 'show']);
+Route::resource('blog', Atin\LaravelBlog\Http\Controllers\PostController::class)
+    ->only(['index', 'show']);
 
-    Route::get('/blog/{post}/image', [Atin\LaravelBlog\Http\Controllers\PostController::class, 'image']);
-});
+Route::get('/blog/{post}/image', [Atin\LaravelBlog\Http\Controllers\PostController::class, 'image']);
